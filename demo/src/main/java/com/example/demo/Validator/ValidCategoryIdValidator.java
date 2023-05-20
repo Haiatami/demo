13e3622a42d87f8dd5a4211b50c94757abdace8e
Admin <haruatami4145@gmail.com>
2023-05-20 16:19:00 +0700

    BÀI TUẦN 3

---
+++ demo/src/main/java/com/example/demo/Validator/ValidCategoryIdValidator.java
@@ -0,0 +1,6 @@
+package com.example.demo.Validator;
+
+import jakarta.validation.ConstraintValidator;
+
+public class ValidCategoryIdValidator implements ConstraintValidator<Va> {
+}
---
+++ demo/src/main/java/com/example/demo/Validator/annotation/ValidCategoryId.java
@@ -0,0 +1,15 @@
+package com.example.demo.Validator.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.reflect.Type;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+//@Target({TYPE, FIELD})
+//@Retention(RUNTIME)
+public class ValidCategoryId {
+}
